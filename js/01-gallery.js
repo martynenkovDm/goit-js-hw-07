09ec5f62442c06be802c6df0298b02c81e6b5b5a
martynenkovDm <126678952+martynenkovDm@users.noreply.github.com>
2023-04-20 20:50:02 +0300

    Add changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+let basicLightboxEl;
+let bigImg;
+const liImgList = galleryItems.map(item => {
+    const { preview, original, description } = item;
 
-console.log(galleryItems);
+    return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        loading="lazy"
+                    />
+                </a>
+            </li>`
+      
+}
+).join('');
+galleryEl.insertAdjacentHTML("beforeend", liImgList);
+galleryEl.addEventListener('click', onImageClick);
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+    bigImg = basicLightbox.create(
+        `<img
+          src="${event.target.dataset.source}"
+          alt="${event.target.alt}"
+        />`,
+    );
+    bigImg.show();
+    basicLightboxEl = document.querySelector('.basicLightbox');
+    document.addEventListener('keydown', onEscBtnClick);
+};
+const onEscBtnClick = (event) => {
+    if (event.code === 'Escape' && basicLightboxEl.classList.contains('basicLightbox--visible')) {
+        bigImg.close();
+    }
+    document.removeEventListener('keydown', onEscBtnClick);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const liImgList = galleryItems.map(item => {
+    const { preview, original, description } = item;
+    return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        alt="${description}"
+                        loading="lazy"
+                    />
+                </a>
+            </li>`
+      
+}).join('');
+galleryEl.insertAdjacentHTML("beforeend", liImgList);
+const bigImg = new SimpleLightbox('.gallery a');
